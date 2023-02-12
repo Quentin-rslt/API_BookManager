6d0620cec8128a4d04a53aa5082e81dc317cbbb0
Quentin Rousselet <quentinr82@protonmail.com>
2023-02-12 12:12:57 +0100

    Delete unused import

--- src/main/java/com/API/BookManager/BookManagerApplication.java
+++ src/main/java/com/API/BookManager/BookManagerApplication.java
@@ -2,9 +2,6 @@ package com.API.BookManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 public class BookManagerApplication {
